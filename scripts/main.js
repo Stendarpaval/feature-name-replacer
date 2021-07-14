f848d1ed116e6dd383affd5c895e509c9049c412
Stendarpaval <stendarpaval@gmail.com>
2021-07-14 19:38:46 +0200

    v0.0.3
    - made the module system agnostic
    - a setting was added that allows you to specify which keywords to replace using a semicolon-separated string
    - a setting was added that allows you to list all the Named Creatures in a semicolon-separated string
    - a setting was added that you can use to change where the module looks for feature description text

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,70 @@
 const moduleName = "feature-name-replacer";
 
+Hooks.on("init", () => {
+	const moduleName = "feature-name-replacer";
+	game.settings.register(moduleName,"replaceKeywords", {
+		name: "SETTINGS.FNR.replaceKeywords",
+		hint: "SETTINGS.FNR.replaceKeywordsHint",
+		config: true,
+		default: "creature;type",
+		scope: "world",
+		type: String
+	});
+
+	game.settings.register(moduleName,"namedCreaturesList", {
+		name: "SETTINGS.FNR.namedCreaturesList",
+		hint: "SETTINGS.FNR.namedCreaturesListHint",
+		config: true,
+		default: "",
+		scope: "world",
+		type: String
+	});
+
+	game.settings.register(moduleName,"descriptionDataPath", {
+		name: "SETTINGS.FNR.descriptionDataPath",
+		hint: "SETTINGS.FNR.descriptionDataPathHint",
+		config: true,
+		default: "data.description.value",
+		scope: "world",
+		type: String
+	});
+});
+
+
 /**
- * Check if the item originates from the Monster Features compendium
- * and if so, replace {creature} and {type} with the creature's name.
+ * Replace {creature} and {type} in the description text with the 
+ * creature's name.
  * If the creature happens to be a Named Creature, then also remove any
  * preceding article ("the").
- * @param {Item5e} [item]			The feature item that is added to the actor
+ * @param {Item} [item]				The feature item that is added to the actor
  * @return {void}					Return nothing to continue
  */
 async function replaceMonsterName(item) {
-	const sourceIdPrefix = "Compendium.dnd5e.monsterfeatures.";
-	if (!item.getFlag("core","sourceId").startsWith(sourceIdPrefix)) return;
-	const sourceId = item.getFlag("core","sourceId").slice(sourceIdPrefix.length);
+	let featureDesc = item.data?.[game.settings.get(moduleName, "descriptionDataPath")];
+	if (!featureDesc) return;
+	let keywords = game.settings.get(moduleName,"replaceKeywords");
+	let splitKeywords = keywords.split(";");
+	for (let i = 0; i < splitKeywords.length; i++) {
+		if (splitKeywords[i] === "") {
+			splitKeywords.splice(i,1);
+			i--;
+		}
+	}
+	let regexString = "";
+	for (let i = 0; i < splitKeywords.length; i++) {
+		if (i === splitKeywords.length - 1) {
+			regexString += `\{${splitKeywords[i]}\}`;	
+		} else {
+			regexString += `\{${splitKeywords[i]}\}|`;
+		}
+	}
+	let regex = new RegExp(regexString,"g");
+	let regexNamedCreature = new RegExp(`(The\\\W|the\\\W)?(${regexString})`,"g");
 	
-	const mfCompendium = await game.packs.get("dnd5e.monsterfeatures");
-	const monsterFeature = mfCompendium.get(sourceId);
-	if (!monsterFeature) return;
-	
-	let featureDesc = monsterFeature.data.data.description.value;
-	let regex = /\{creature\}|\{type\}/g;
-	if (item.actor.getFlag(moduleName, "namedCreature") || item.actor.type === "character") {
-		regex = /(The\W|the\W)?(\{creature\}|\{type\})/g;
-		featureDesc = featureDesc.replaceAll(regex, item.actor.name);
+	// let regex = /\{creature\}|\{type\}/g;
+	if (item.actor.getFlag(moduleName, "namedCreature") || item.actor.type === "character" || checkNamedCreatureList(item)) {
+		// regex = /(The\W|the\W)?(\{creature\}|\{type\})/g;
+		featureDesc = featureDesc.replaceAll(regexNamedCreature, item.actor.name);
 	} else if (item.actor.getFlag(moduleName, "namedCreature") === undefined) {
 		await item.actor.setFlag(moduleName, "namedCreature", false);
 		featureDesc = featureDesc.replaceAll(regex, item.actor.name.toLowerCase());
@@ -29,7 +72,7 @@ async function replaceMonsterName(item) {
 		featureDesc = featureDesc.replaceAll(regex, item.actor.name.toLowerCase());
 	}
 
-	if (item.name !== monsterFeature.name) return;
+	// if (item.name !== monsterFeature.name) return;
 	await item.update({"data.description.value": featureDesc});
 }
 
@@ -38,10 +81,12 @@ async function replaceMonsterName(item) {
  * the ActorTypeConfig application.
  * @param {ActorTypeConfig} [args]	The ActorTypeConfig application data
  * @param {string} [html]			The rendered html of the ActorTypeConfig application
+ * @return {void}					Return nothing to continue
  */
 async function injectNamedCreatureOption(args, html) {
+	if (game.system.id !== "dnd5e") return;
 	let data = {value: false};
-	if (args.object.getFlag(moduleName, "namedCreature")  || item.actor.type === "character") {
+	if (args.object.getFlag(moduleName, "namedCreature") || args.object.type === "character") {
 		data.value = true;
 	} else if (args.object.getFlag(moduleName, "namedCreature") === undefined) {
 		await args.object.setFlag(moduleName, "namedCreature", false);
@@ -56,12 +101,35 @@ async function injectNamedCreatureOption(args, html) {
  * application is closed.
  * @param {ActorTypeConfig} [args]	The ActorTypeConfig application data
  * @param {string} [html]			The rendered html of the ActorTypeConfig application
+ * @return {void}					Return nothing to continue
  */
 async function detectNamedCreatureOption(args, html) {
+	if (game.system.id !== "dnd5e") return;
 	if (html.find('input[name="namedCreature"]')[0].checked) {
 		await args.object.setFlag(moduleName, "namedCreature", true);
 	} else {
 		await args.object.setFlag(moduleName, "namedCreature", false);
+	}
+}
+
+/**
+ * Checks the list of named creatures against the item owner's name.
+ * @param {Item} [item]				The item being added to the actor sheet
+ * @returns {boolean}				Whether or not the named creature list includes the item owner's name
+ */
+function checkNamedCreatureList(item) {
+	const namedCreaturesList = game.settings.get(moduleName, "namedCreaturesList");
+	let splitNames = namedCreaturesList.split(";");
+	for (let i = 0; i < splitNames.length; i++) {
+		if (splitNames[i] === "") {
+			splitNames.splice(i,1);
+			i--;
+		}
+	}
+	if (splitNames.includes(item.parent.name)) {
+		return true;
+	} else {
+		return false;
 	}
 }
 
